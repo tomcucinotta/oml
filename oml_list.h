a32c5d683a220acbc8676448435e43ba73482020
Tommaso Cucinotta <tommaso.cucinotta@santannapisa.it>
2007-09-02 15:58:47 +0000

    Added both const_iterator and iterator for oml_list. Added oml_list_insert() and oml_list_remove() and oml_list_del() acting through the non-const list iterator.

--- oml_list.h
+++ oml_list.h
@@ -137,11 +137,11 @@
 #define oml_list_cleanup(this) \
   oml_list_clear(this)
 
-/* FORWARD ITERATOR */
+/* FORWARD CONST ITERATOR */
 
 /** Get an iterator positioned on the head element
  **/
-#define oml_list_begin(this, p_it) ({ \
+#define oml_list_const_begin(this, p_it) ({ \
   oml_rv __rv = OML_OK; \
   do { \
     *(p_it) = (this)->p_head; \
@@ -150,8 +150,75 @@
 })
 
 /** Check if the iterator is dereferenceable (i.e. we may call oml_list_get() or oml_list_value()). **/
-#define oml_list_has_value(this, p_it) \
+#define oml_list_const_has_value(this, p_it) \
   (*(p_it) != NULL)
+
+/** Move the iterator one element forward. */
+#define oml_list_const_next(this, p_it) ({ \
+  oml_rv __rv = OML_OK; \
+  do { \
+    if (! oml_list_const_has_value((this), (p_it))) { \
+      __rv = OML_E_NOT_FOUND; \
+      break; \
+    } \
+    *(p_it) = (*(p_it))->p_next; \
+  } while (0); \
+  __rv; \
+})
+
+/** Retrieve and store in p_value the element referenced to by the iterator p_it.
+ **
+ ** @return OML_E_NOT_FOUND if the iterator is past the last element in the list.
+ **/
+#define oml_list_const_get(this, p_it, p_value) ({ \
+  oml_rv __rv = OML_OK; \
+  do { \
+    if (! oml_list_const_has_value((this), (p_it))) { \
+      __rv = OML_E_NOT_FOUND; \
+      break; \
+    } \
+    *(p_value) = (*(p_it))->value; \
+  } while (0); \
+  __rv; \
+})
+
+/** Direct (L-assignable) reference to the value referenced by p_it.
+ **
+ ** @note If the iterator is not valid, behaviour is unspecified.
+ **/
+#define oml_list_const_value(this, p_it) ((*(p_it))->value)
+
+/** Get an iterator positioned past the last element. **/
+#define oml_list_const_end(this, p_it) ({ \
+  oml_rv __rv = OML_OK; \
+  do { \
+    *(p_it) = NULL; \
+  } while (0); \
+  __rv; \
+})
+
+/** Check if the two iterators refer to the same position **/
+#define oml_list_const_iter_eq(this, p_it1, p_it2) \
+  ((*(p_it1)) == (*(p_it2)))
+
+/** Let the supplied iterator iterate over the entire collection **/
+#define oml_list_const_foreach(this, p_it)	\
+  for (oml_list_const_begin(this, p_it); oml_list_const_has_value(this, p_it); oml_list_const_next(this, p_it))
+
+/* FORWARD R/W ITERATOR */
+
+/** Get an iterator positioned on the head element **/
+#define oml_list_begin(this, p_it) ({ \
+  oml_rv __rv = OML_OK; \
+  do { \
+    *(p_it) = NULL; \
+  } while (0); \
+  __rv; \
+})
+
+/** Check if the iterator is dereferenceable (i.e. we may call oml_list_get() or oml_list_value()). **/
+#define oml_list_has_value(this, p_it) \
+  ( (*(p_it) != NULL && (*(p_it))->p_next != NULL) || (*(p_it) == NULL && (this)->p_head != NULL) )
 
 /** Move the iterator one element forward. */
 #define oml_list_next(this, p_it) ({ \
@@ -161,7 +228,10 @@
       __rv = OML_E_NOT_FOUND; \
       break; \
     } \
-    *(p_it) = (*(p_it))->p_next; \
+    if (*(p_it) == NULL) \
+      *(p_it) = (this)->p_head;	\
+    else \
+      *(p_it) = (*(p_it))->p_next; \
   } while (0); \
   __rv; \
 })
@@ -177,7 +247,10 @@
       __rv = OML_E_NOT_FOUND; \
       break; \
     } \
-    *(p_value) = (*(p_it))->value; \
+    if (*(p_it) == NULL) \
+      *(p_value) = (this)->p_head->value; \
+    else \
+      *(p_value) = (*(p_it))->p_next->value; \
   } while (0); \
   __rv; \
 })
@@ -186,13 +259,80 @@
  **
  ** @note If the iterator is not valid, behaviour is unspecified.
  **/
-#define oml_list_value(this, p_it) ((*(p_it))->value)
+#define oml_list_value(this, p_it) ({ \
+  typeof((*(p_it))->value) *__p_value; \
+  if (*(p_it) == NULL) \
+    __p_value = &(this)->p_head->value; \
+  else \
+    __p_value = &(*(p_it))->p_next->value; \
+  *__p_value; \
+})
 
 /** Get an iterator positioned past the last element. **/
 #define oml_list_end(this, p_it) ({ \
   oml_rv __rv = OML_OK; \
   do { \
-    *(p_it) = NULL; \
+    *(p_it) = (this)->p_tail; \
+  } while (0); \
+  __rv; \
+})
+
+/** Insert a value at the current iterator position in the list.
+ **
+ ** The element is inserted before the one currently referenced by the iterator,
+ ** so that, after the insertion, the element returned by oml_list_get() is
+ ** the new inserted value.
+ **/
+#define oml_list_insert(this, p_it, _value) ({ \
+  oml_rv __rv = OML_OK; \
+  typeof((this)->p_head->value) __value = (_value); \
+  do { \
+    typeof((this)->p_head) __p_node = oml_malloc(sizeof(typeof(*(this)->p_head))); \
+    if (__p_node == NULL) { \
+      __rv = OML_E_NO_MEMORY; \
+      break; \
+    } \
+    __p_node->value = __value; \
+    if (*(p_it) == NULL) { \
+      __p_node->p_next = (this)->p_head; \
+      (this)->p_head = __p_node; \
+    } else { \
+      __p_node->p_next = (*(p_it))->p_next; \
+      (*(p_it))->p_next = __p_node; \
+    } \
+    if (__p_node->p_next == NULL) \
+      (this)->p_tail = __p_node; \
+    (this)->num_elems++; \
+  } while (0); \
+  __rv; \
+})
+
+/** Removes the value currently referenced by the iterator */
+#define oml_list_remove(this, p_it) ({ \
+  oml_rv __rv = OML_OK; \
+  do { \
+    typeof((this)->p_head) __p_node; \
+    if (oml_list_empty(this)) { \
+      __rv = OML_E_EMPTY; \
+      break; \
+    } \
+    if (! oml_list_has_value((this), (p_it))) { \
+      __rv = OML_E_INCONSISTENT_STATE; \
+      break; \
+    } \
+    if (*(p_it) == NULL) { \
+      __p_node = (this)->p_head; \
+      (this)->p_head = __p_node->p_next; \
+      if ((this)->p_tail == __p_node) \
+        (this)->p_tail = NULL; \
+    } else { \
+      __p_node = (*(p_it))->p_next; \
+      (*(p_it))->p_next = __p_node->p_next; \
+      if ((this)->p_tail == __p_node) \
+        (this)->p_tail = *(p_it); \
+    } \
+    (this)->num_elems--; \
+    oml_free(__p_node); \
   } while (0); \
   __rv; \
 })
@@ -204,5 +344,19 @@
 /** Let the supplied iterator iterate over the entire collection **/
 #define oml_list_foreach(this, p_it)	\
   for (oml_list_begin(this, p_it); oml_list_has_value(this, p_it); oml_list_next(this, p_it))
+
+#include "oml_list_find.h"
+
+#define oml_list_del(this, _value) ({ \
+  oml_rv __rv = OML_OK; \
+  do { \
+    typeof((this)->p_head) it; \
+    __rv = oml_find((this), _value, &it); \
+    if (__rv != OML_OK) \
+      break; \
+    __rv = oml_remove((this), &it); \
+  } while (0); \
+  __rv; \
+})
 
 #endif /* __OML_LIST_H__ */
--- test-list.c
+++ test-list.c
@@ -12,13 +12,178 @@ oml_list(int) h;
 #define SIZE 4
 int values[SIZE];
 
+/** Insert elements with push_back and verify they are in the same order **/
+void test_const_iterator() {
+  oml_list(int) l;
+  oml_list_iterator(int) it;
+  int i, v;
+
+  oml_chk_ok_exit(oml_list_init(&l));
+  for (i = 0; i < SIZE; ++i) {
+    oml_chk_ok_exit(oml_list_push_back(&l, values[i]));
+    printf("Size of list: %d\n", oml_list_size(&l));
+    oml_chk_exit(oml_list_size(&l) == i + 1);
+  }
+  oml_chk_ok_exit(oml_list_const_begin(&l, &it));
+  for (i = 0; i < SIZE; ++i) {
+    oml_chk_ok(oml_list_const_get(&l, &it, &v));
+    printf("Value: %d\n", v);
+    oml_chk_exit(v == values[i]);
+    oml_list_const_next(&l, &it);
+  }
+  oml_chk_ok_exit(oml_list_cleanup(&l));
+}
+
+/** Insert elements with push_front and verify they are in reversed order **/
+void test_const_iterator_rev() {
+  oml_list(int) l;
+  oml_list_iterator(int) it;
+  int i, v;
+
+  oml_chk_ok_exit(oml_list_init(&l));
+  for (i = 0; i < SIZE; ++i) {
+    oml_chk_ok_exit(oml_list_push_front(&l, values[i]));
+    printf("Size of list: %d\n", oml_list_size(&l));
+    oml_chk_exit(oml_list_size(&l) == i + 1);
+  }
+  oml_chk_ok_exit(oml_list_const_begin(&l, &it));
+  for (i = 0; i < SIZE; ++i) {
+    oml_chk_ok(oml_list_const_get(&l, &it, &v));
+    printf("Value: %d\n", v);
+    oml_chk_exit(v == values[SIZE - 1 - i]);
+    oml_list_const_next(&l, &it);
+  }
+  oml_chk_ok_exit(oml_list_cleanup(&l));
+}
+
+void test_iterator() {
+  oml_list(int) l;
+  oml_list_iterator(int) it;
+  int i, v;
+
+  oml_chk_ok_exit(oml_list_init(&l));
+  for (i = 0; i < SIZE; ++i) {
+    oml_chk_ok_exit(oml_list_push_back(&l, values[i]));
+    printf("Size of list: %d\n", oml_list_size(&l));
+    oml_chk_exit(oml_list_size(&l) == i + 1);
+  }
+  oml_chk_ok_exit(oml_list_begin(&l, &it));
+  for (i = 0; i < SIZE; ++i) {
+    oml_chk_ok(oml_list_get(&l, &it, &v));
+    printf("Value: %d\n", v);
+    oml_chk_exit(v == values[i]);
+    oml_list_next(&l, &it);
+  }
+  oml_chk_ok_exit(oml_list_cleanup(&l));
+}
+
+void test_iterator_rev() {
+  oml_list(int) l;
+  oml_list_iterator(int) it;
+  int i, v;
+
+  oml_chk_ok_exit(oml_list_init(&l));
+  for (i = 0; i < SIZE; ++i) {
+    oml_chk_ok_exit(oml_list_push_front(&l, values[i]));
+    printf("Size of list: %d\n", oml_list_size(&l));
+    oml_chk_exit(oml_list_size(&l) == i + 1);
+  }
+  oml_chk_ok_exit(oml_list_begin(&l, &it));
+  for (i = 0; i < SIZE; ++i) {
+    oml_chk_ok(oml_list_get(&l, &it, &v));
+    printf("Value: %d\n", v);
+    oml_chk_exit(v == values[SIZE - 1 - i]);
+    oml_list_next(&l, &it);
+  }
+  oml_chk_ok_exit(oml_list_cleanup(&l));
+}
+
+/** Verify elements are in the same order as values[] **/
+void test_values_forward(oml_list(int) *pl) {
+  oml_list_iterator(int) it;
+  int i, v;
+  oml_chk_ok_exit(oml_list_const_begin(pl, &it));
+  for (i = 0; i < SIZE; ++i) {
+    oml_chk_ok(oml_list_const_get(pl, &it, &v));
+    printf("Value: %d\n", v);
+    oml_chk_exit(v == values[i]);
+    oml_chk_exit(oml_list_const_value(pl, &it) == values[i]);
+    oml_list_const_next(pl, &it);
+  }
+  oml_chk_exit(! oml_list_const_has_value(pl, &it));
+}
+
+/** Verify elements are in reversed order w.r.t. values[] **/
+void test_values_backward(oml_list(int) *pl) {
+  oml_list_iterator(int) it;
+  int i, v;
+  oml_chk_ok_exit(oml_list_const_begin(pl, &it));
+  for (i = 0; i < SIZE; ++i) {
+    oml_chk_ok(oml_list_const_get(pl, &it, &v));
+    printf("Value: %d\n", v);
+    oml_chk_exit(v == values[SIZE - 1 - i]);
+    oml_chk_exit(oml_list_const_value(pl, &it) == values[SIZE - 1 - i]);
+    oml_list_const_next(pl, &it);
+  }
+  oml_chk_exit(! oml_list_const_has_value(pl, &it));
+}
+
+/** Insert elements with insert always at begin and always at last, and
+ ** verify they are, respectively, in the same and reversed order.
+ **
+ ** Also, delete elements using an iterator always at begin
+ ** and verify they are deleted, respectively, in the same and reversed order.
+ **/
+void test_insert() {
+  oml_list(int) l;
+  oml_list_iterator(int) it;
+  int i, v;
+
+  oml_chk_ok_exit(oml_list_init(&l));
+  oml_chk_ok_exit(oml_list_begin(&l, &it));
+  for (i = 0; i < SIZE; ++i) {
+    oml_chk_ok_exit(oml_list_insert(&l, &it, values[i]));
+    oml_chk_ok_exit(oml_list_get(&l, &it, &v));
+    oml_chk_exit(v == values[i]);
+  }
+  test_values_backward(&l);
+
+  for (i = 0; i < SIZE; ++i) {
+    oml_chk_ok_exit(oml_list_get(&l, &it, &v));
+    oml_chk_exit(v == values[SIZE - 1 - i]);
+    oml_chk_ok_exit(oml_list_remove(&l, &it));
+  }
+  oml_chk_exit(oml_list_empty(&l));
+  oml_chk_ok_exit(oml_list_cleanup(&l));
+
+  oml_chk_ok_exit(oml_list_begin(&l, &it));
+  for (i = 0; i < SIZE; ++i) {
+    oml_chk_ok_exit(oml_list_insert(&l, &it, values[i]));
+    oml_chk_ok_exit(oml_list_get(&l, &it, &v));
+    oml_chk_exit(v == values[i]);
+    oml_chk_ok_exit(oml_list_next(&l, &it));
+  }
+  test_values_forward(&l);
+
+  oml_chk_ok_exit(oml_list_begin(&l, &it));
+  for (i = 0; i < SIZE; ++i) {
+    oml_chk_ok_exit(oml_list_get(&l, &it, &v));
+    oml_chk_exit(v == values[i]);
+    oml_chk_ok_exit(oml_list_remove(&l, &it));
+  }
+  oml_chk_exit(oml_list_empty(&l));
+  oml_chk_ok_exit(oml_list_cleanup(&l));
+}
+
 int main(int argc, char **argv) {
   int v;
+  int i;
 
   oml_chk_ok_exit(oml_list_init(&h));
 
   printf("Size of list: %d\n", oml_list_size(&h));
   oml_chk_exit(oml_list_size(&h) == 0);
+
   oml_chk_ok_exit(oml_list_push_front(&h, 1024));
   printf("Size of list: %d\n", oml_list_size(&h));
   oml_chk_exit(oml_list_size(&h) == 1);
@@ -54,7 +219,6 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  int i;
   oml_list_iterator(int) it;
 
   for (i = 0; i < SIZE; ++i) {
@@ -66,7 +230,7 @@ int main(int argc, char **argv) {
   printf("List dump:\n");
   oml_list_begin(&h, &it);
   for (; oml_list_has_value(&h, &it); oml_list_next(&h, &it)) {
-    oml_chk_ok_exit(oml_list_get_next(&h, &it, &v));
+    oml_chk_ok_exit(oml_list_get(&h, &it, &v));
     printf("  List elem: %d\n", v);
   }
 
@@ -83,8 +247,16 @@ int main(int argc, char **argv) {
 
   oml_chk_ok_exit(oml_list_cleanup(&h));
 
-  oml_chk_exit(oml_mem_clean());
+  test_iterator();
+  test_const_iterator();
 
+  test_iterator_rev();
+  test_const_iterator_rev();
+
+  test_insert();
+  test_del();
+
+  oml_chk_exit(oml_mem_clean());
   printf("Test successful\n");
   return 0;
 }
