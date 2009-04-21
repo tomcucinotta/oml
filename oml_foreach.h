40b9ee0247ce7adf7825075dfb9373539064c1f0
Tommaso Cucinotta <tommaso.cucinotta@santannapisa.it>
2009-04-21 16:03:32 +0000

    Added foreach algorithm, working on any type of container.

---
+++ oml_foreach.h
@@ -0,0 +1,21 @@
+#ifndef __OML_FOREACH__
+#define __OML_FOREACH__
+
+/** Example of usage:
+ **
+ ** @code
+ **   oml_list(int) l;
+ **   oml_list_iterator(int) it, it_end;
+ **   oml_list_begin(&l, &it);
+ **   oml_list_end(&l, &it);
+ **   oml_foreach(list, &l, &it, &it_end) {
+ **     int value;
+ **     oml_list_get(&l, &it, &v);
+ **     /* Use value */
+ **   }
+ **   @endcode
+ **/
+#define oml_foreach(type, this, p_it, p_end) \
+  for (; ! oml_ ##type## _iter_eq((this), (it), (end_it)); oml_ ##type## _next(it))
+
+#endif /* __OML_FOREACH__ */
