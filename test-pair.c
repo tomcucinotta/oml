a9debaf5de0ec6f5c43c1be158ff20dd816b2b57
Remo <andreoliremo@gmail.com>
2023-08-04 17:25:11 +0200

    test-pair: test different pair types in same compilation

--- test-pair.c
+++ test-pair.c
@@ -4,10 +4,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+typedef char* CHARP;
 oml_define_pair(int, double);
+oml_define_pair(CHARP, int);
 
 oml_pair(int, double) h;
+oml_pair(CHARP, int) h2;
 
 int main(int argc, char **argv) {
   oml_chk_ok_exit(oml_pair_init(&h, 3, 4.0));
@@ -17,6 +21,10 @@ int main(int argc, char **argv) {
   oml_pair_second(&h) = 7.5;
   oml_chk_exit(oml_pair_first(&h) == 7);
   oml_chk_exit(oml_pair_second(&h) == 7.5);
+
+  oml_chk_ok_exit(oml_pair_init(&h2, "x", 4));
+  oml_chk_exit(strcmp(oml_pair_first(&h2), "x") == 0);
+  oml_chk_exit(oml_pair_second(&h2) == 4);
 
   printf("Test successful\n");
   return 0;
