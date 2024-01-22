ed894fe7e354daf9d359ae69517e8e5b4a5130d8
Remo <andreoliremo@gmail.com>
2024-01-22 19:18:05 +0100

    oml_sync: minor tweaks

--- oml_sync.h
+++ oml_sync.h
@@ -8,25 +8,33 @@
 
 /**
  * @file
- * 
+ * Collections of wrappers for multi-thread support
  * @brief 
  */
 
 #define oml_mutex pthread_mutex_t
 
-#define oml_sync(p_mtx, rv, name, params...)    \
-  rv name##__(params);                          \
-  static rv name(params) {                      \
+#define oml_sync(p_mtx, rv, function_name, params...)    \
+  rv function_name##__(params);                          \
+  static rv function_name(params) {                      \
     void *__func_args, *__func_rv;              \
     pthread_mutex_lock(p_mtx);			\
     oml_log_debug("Acquired lock : *" #p_mtx);	\
     __func_args = __builtin_apply_args();       \
-    __func_rv = __builtin_apply((__oml_void_func) name##__, __func_args, OML_FUNC_MAX_ARGS_SIZE); \
+    __func_rv = __builtin_apply((__oml_void_func) function_name##__, __func_args, OML_FUNC_MAX_ARGS_SIZE); \
     oml_log_debug("Releasing lock: *" #p_mtx);	\
     pthread_mutex_unlock(p_mtx);		\
     __builtin_return(__func_rv);                \
   }                                             \
-  rv name##__(params) 
+  rv function_name##__(params) 
 
+#define oml_sync_region(p_mutex)			\
+  auto void __sync(void);			\
+  pthread_mutex_lock(p_mutex);			\
+  oml_log_debug("Acquired mutex : *" #p_mutex);	\
+  __sync();					\
+  oml_log_debug("Releasing mutex: *" #p_mutex);	\
+  pthread_mutex_unlock(p_mutex);		\
+  void __sync(void) 
 
 #endif /* __OML_SYNC_H__ */
--- test-sync-nest.c
+++ test-sync-nest.c
@@ -1,22 +1,12 @@
 #include <pthread.h>
 #define OML_DEBUG_LEVEL OML_LEVEL_DEBUG
 #include "oml_debug.h"
-
-#include <pthread.h>
-
-#define oml_sync(p_mutex)			\
-  auto void __sync(void);			\
-  pthread_mutex_lock(p_mutex);			\
-  oml_log_debug("Acquired mutex : *" #p_mutex);	\
-  __sync();					\
-  oml_log_debug("Releasing mutex: *" #p_mutex);	\
-  pthread_mutex_unlock(p_mutex);		\
-  void __sync(void) 
+#include "oml_sync.h"
 
 int main(int argc, char *argv[]) {
   pthread_mutex_t m;
 
-  oml_sync(&m) {
+  oml_sync_region(&m) {
     printf("args=%d\n", argc);
     return;
   }
