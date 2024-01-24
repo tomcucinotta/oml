70b89a5e962898cdd36d2e08c2bcda0f958f0845
Tommaso Cucinotta <tommaso.cucinotta@santannapisa.it>
2024-01-24 10:46:22 +0100

    test-sync*.c: verifying the mutex is actually acquired within a sync function/block

--- test-sync-nest.c
+++ test-sync-nest.c
@@ -2,11 +2,16 @@
 #define OML_DEBUG_LEVEL OML_LEVEL_DEBUG
 #include "oml_debug.h"
 #include "oml_sync.h"
+#include <errno.h>
 
 int main(int argc, char *argv[]) {
-  pthread_mutex_t m;
+  pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 
   oml_sync_region(&m) {
+    int err;
+    if ((err = pthread_mutex_trylock(&m)) != 0)
+      oml_log_debug("pthread_mutex_trylock() failed as expected: %d", err);
+    oml_chk(err == EBUSY);
     printf("args=%d\n", argc);
     return;
   }
--- test-sync.c
+++ test-sync.c
@@ -1,10 +1,15 @@
 #define OML_DEBUG_LEVEL OML_LEVEL_DEBUG
 #include "oml_sync.h"
 #include <stdio.h>
+#include <errno.h>
 
-oml_mutex m;
+oml_mutex m = PTHREAD_MUTEX_INITIALIZER;
 
 oml_sync(&m, int, f, int a, int b) {
+  int err;
+  if ((err = pthread_mutex_trylock(&m)) != 0)
+    oml_log_debug("pthread_mutex_trylock() failed as expected: %d", err);
+  oml_chk(err == EBUSY);
   oml_log_debug("Computing %d + %d", a, b);
   if (a == 0 && b == 0)
     return 0;
